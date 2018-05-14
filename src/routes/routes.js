02b7a5092e74008197bd01acf8d6b99ebd12ccea
Kevin (Kun) "Kassimo" Qian <kevinkassimo@gmail.com>
2018-05-14 13:35:38 -0700

    ...Yet again

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import qs from 'qs';
 import NotificationsSystem from 'reapop';
 import theme from 'reapop-theme-wybo';
